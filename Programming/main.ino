bada8e5aea0879caab33c106a199a141b9504aeb
Bryan <bryankearneyiii@gmail.com>
2022-12-27 16:04:13 -0500

    Update main.ino

--- Programming/main.ino
+++ Programming/main.ino
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <SoftwareSerial.h>
 
 #define CLKPIN 0
 #define DATAPIN 0
 #define LATCHPIN 0
+
+#define BTRXPIN 0
+#define BTTXPIN 0
 
 // TYPE DECLARATIONS
 typedef unsigned char uint8_t, byte;
@@ -17,6 +20,7 @@ typedef int int32_t;
 typedef long int64_t;
 typedef long long int128_t;
 
+SoftwareSerial BluetoothModule(BTRXPIN, BTTXPIN); // RX, TX
 /*
 ? queries - https://ccrma.stanford.edu/~fgeorg/250a/lab2/arduino-0019/reference/PortManipulation.html
 * B - 8:13 (PORTB, DDRB, PINB) (two high bits, 6 & 7, are not usable)
@@ -24,11 +28,22 @@ typedef long long int128_t;
 * D - 0:7 (PORTD, DDRD, PIND) (0 and 1 are tx and rx respectively)
 */
 
+typedef enum {
+	movementOPC 	= 0x0,
+	launchOPC	= 0x1,
+	modifySpeedOPC	= 0x2
+} opcodes;
+
 // Kearney 12.7.22
 #define GET(STREAM, N) STREAM & (1 << N)
 #define SET(STREAM, N) STREAM |= 1 << (N)
 #define CLR(STREAM, N) STREAM &= ~(1 << (N))
 
+// Kearney 12.15.22
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define SQR(x) ((x) * (x))
+
+// Kearney 12.7.22
 // SET/CLR without changing 1st or 2nd bits
 // #define SETEXC1N2(STREAM, N) SET(STREAM, N) & 0b11111100
 // #define CLREXC1N2(STREAM, N) CLR(STREAM, N) & 0b11111100
@@ -106,6 +121,30 @@ uint8_t outputPin[4] = { 	// The shift register pins to be pulsed
 	1 << 7	// Back Left
 }
 
+typedef enum {
+	frontRightRev = 0,
+	frontLeftRev,
+	backRightRev,
+	backLeftRev
+} revPins;
+
+inline void setReverse(uint8_t index, bool rev) {
+	switch (index) {
+		case (frontRightRev):
+			outputPin[0] = rev ? 1 << 1 : 1 << 0;
+			break;
+		case (frontLeftRev):
+			outputPin[1] = rev ? 1 << 2 : 1 << 3;
+			break;
+		case (backRightRev):
+			outputPin[2] = rev ? 1 << 5 : 1 << 4;
+			break;
+		case (backLeftRev):
+			outputPin[3] = rev ? 1 << 6 : 1 << 7;
+			break;
+	}
+}
+
 // GND/VCC on motor drivers from shift register:
 /*
 2Q4 - MMRF VCC
@@ -133,6 +172,7 @@ inline void shiftOutFast(uint8_t dataPin, uint8_t clkPin, uint16_t data) {
 		/ send bit to data pin
 		/ clk high
 		/ clk low
+		/ shift data >> 1
 	}
 	*/
 
@@ -140,7 +180,8 @@ inline void shiftOutFast(uint8_t dataPin, uint8_t clkPin, uint16_t data) {
 }
 
 // Individual motor speed calculations
-uint8_t launcherRightSpeed,
+// Smaller is faster, larger is significantly slower
+uint32_t launcherRightSpeed,
 	launcherLeftSpeed,
 	frontRightSpeed,
 	frontLeftSpeed,
@@ -194,11 +235,86 @@ void pulseDuePWM() {
 	shiftOutFast(DATAPIN, CLKPIN, PWMData);
 }
 
+// Kearney 12.15.22
+inline void setSpeed(uint32_t* speedVariable, uint16_t speed) {
+	*speedVariable = (16 * SQR(ABS(speed))) / 1000;
+}
+
+inline void config() {
+	Serial.begin(9600); // enable serial with data exchange rate of 9600
+	BluetoothModule.begin(9600); // enable bluetooth module with data exchange rate of 9600
+}
+
 void setup() {
 	
 }
 
+
+
+uint64_t lastRec = millis();
 void loop() {
+	if (BluetoothModule.available() >= 4) {
+		// if 4 bytes are able to be read
+		uint8_t instruction = BluetoothModule.read();
+		int8_t data[3];
+		for (int i = 0; i < 3; i++) {
+			data[i] = BluetoothModule.read();
+		}
+
+		// reset reverse data
+		setReverse(frontRightRev, false);
+		setReverse(frontLeftRev, false);
+		setReverse(backRightRev, false);
+		setReverse(backLeftRev, false);
+
+		switch (instruction) {
+			case (movementOPC):
+				// change motors to reverse mode, execute speed functions, etc.
+				int8_t X = data[0] * 6.35, Z = data[1] * 6.35;
+				int8_t midPoint = (X + Z) / 2;
+
+				if (X > 0) {
+					if (Z > 0) {
+						// move forward right
+					} elseif (Z == 0) {
+						// turn right
+					} elseif (Z < 0) {
+						// move backwards right
+					}
+				} elseif (X == 0) {
+					if (Z > 0) {
+						// move forward
+					} elseif (Z == 0) {
+						// dont move
+					} elseif (Z < 0) {
+						// move backwards	
+					}
+				} elseif (X < 0) {
+					if (Z > 0) {
+						// move forward left
+					} elseif (Z == 0) {
+						// turn left
+					} elseif (Z < 0) {
+						// move back left
+						setSpeed(&frontLeftWheel, (int8_t)(Z / 3));
+						setSpeed(&backRightWheel, (int8_t)(Z / 3));
+						backLeftWheel = 0;
+						frontRightWheel = 0;
+
+						setReverse(frontLeftRev, true);
+						setReverse(backRightRev, true);
+					}
+				}
+
+				break;
+			case (launchOPC):
+				break;
+			case (modifySpeedOPC):
+				break
+			default:
+				break;
+		}
+	}
 	pulseDuePWM();
 	delayMicroseconds(100); // slow and steady wins the race
 }
